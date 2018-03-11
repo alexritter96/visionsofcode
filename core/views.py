00ab28cefab6bee29fc9369def681f996144bf1f
Alex Ritter <alexanderritter@comcast.net>
2018-03-11 01:39:48 -0500

    first commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    posts = Blog.objects.order_by('-posted')[:5]
+    context  = {'posts': posts}
+    return render_to_response('core/index.html', context)
+
+
+def view_post(request, slug):
+    return render_to_response('core/post.html', {
+        'posts': get_object_or_404(Blog, slug=slug)
+    })
+
+
+
+
