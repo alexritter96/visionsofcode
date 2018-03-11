d4be787d16a9695543ad845fa0cd6440f34f82ca
Alex Ritter <alexanderritter@comcast.net>
2018-03-11 03:33:31 -0400

    changed base.html file

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from .models import Blog
 # Create your views here.
 def index(request):
     posts = Blog.objects.order_by('-posted')[:5]
-    context  = {'posts': posts}
+    context = {'posts': posts}
     return render_to_response('core/index.html', context)
 
 
@@ -12,7 +12,4 @@ def view_post(request, slug):
     return render_to_response('core/post.html', {
         'posts': get_object_or_404(Blog, slug=slug)
     })
-
-
-
 
