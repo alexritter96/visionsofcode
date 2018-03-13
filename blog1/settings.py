d2b2f50f7f55d41f6ee903f6a4be3e3d9948950c
Alex Ritter <alexanderritter@comcast.net>
2018-03-12 20:14:32 -0400

    add dashboard

--- blog1/settings.py
+++ blog1/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core',
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog1/urls.py
+++ blog1/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('jet', include('jet.urls', 'jet')),
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
 ]
